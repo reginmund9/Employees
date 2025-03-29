bb1a715bace2a75cda8c0997d3c1c1a4c82b408d
reginmund9 <moronitaca@gmail.com>
2025-03-29 13:20:33 -0600

    style

--- MvcEmployees/obj/Debug/net9.0/MvcEmployees.AssemblyInfo.cs
+++ MvcEmployees/obj/Debug/net9.0/MvcEmployees.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcEmployees")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49db87c31420b8645aca90d5a5d2c870e9520b5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3c7a43417bd1227a5b93e1d54d113f97376f9df")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcEmployees")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcEmployees")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
